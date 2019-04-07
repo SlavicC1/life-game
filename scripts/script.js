a1b7fc6d96e444ef8b3c29f880c1ef9b71bb29bb
SlavicC1 <v.shinckaruck@yandex.ru>
2019-04-07 22:15:37 +0300

    add game and control logic

--- scripts/script.js
+++ scripts/script.js
@@ -6,18 +6,142 @@ var count = 10;
 var size = img.width/count;
 img.height = img.width;
 
-for (i = 0; i < count; i ++)
+var counter = 0;
+var gameState = 0;
+
+function SetFreeGreed()
 {
-    cells[i] = [];
-    for (j = 0; j < count; j ++) 
+    for (i = 0; i < count; i ++)
     {
-        cells[i][j] = 0;
+        cells[i] = [];
+        for (j = 0; j < count; j ++) 
+        {
+            cells[i][j] = 0;
+        }
     }
 }
+SetFreeGreed();
 
-function Update()
+function GameStep()
 {
+    var oldCells = [];
+    for (i = 0; i < count; i ++)
+    {
+        oldCells[i] = [];
+        for (j = 0; j < count; j ++) 
+        {
+            oldCells[i][j] = cells[i][j];
+        }
+    }
+
+    for (i = 0; i < count; i ++)
+    for (j = 0; j < count; j ++) 
+    {
+        counter = 0;
+        if(i > 0)
+        {
+            if(cells[i-1][j] == 1)
+            {
+                counter++;
+            }
+            if(j>0)
+            {
+                if(cells[i-1][j-1] == 1)
+                {
+                    counter++;
+                }
+            }
+            if(j < count - 1)
+            {
+                if(cells[i-1][j+1] == 1)
+                {
+                    counter++;
+                }
+            }
+        }
+        if(i < count - 1)
+        {
+            if(cells[i+1][j] == 1)
+            {
+                counter++;
+            }
+            if(j>0)
+            {
+                if(cells[i+1][j-1] == 1)
+                {
+                    counter++;
+                }
+            }
+            if(j < count - 1)
+            {
+                if(cells[i+1][j+1] == 1)
+                {
+                    counter++;
+                }
+            }
+        }
+        if(j > 0)
+        {
+            if(cells[i][j-1] == 1)
+            {
+                counter++;
+            }
+        }
+        if(j < count - 1)
+        {
+            if(cells[i][j+1] == 1)
+            {
+                counter++;
+            }
+        }
+        
+        if(counter >= 4 || counter <= 1)
+        {
+            oldCells[i][j] = 0;
+        }
+        
+        if(counter == 3)
+        {
+            oldCells[i][j] = 1;
+        }
+        
+    }
+
+    for (i = 0; i < count; i ++)
+    {
+        for (j = 0; j < count; j ++) 
+        {
+            cells[i][j] = oldCells[i][j];
+        }
+    }
+
+}
+
+function Stop()
+{
+    gameState = 0;
+    SetFreeGreed();
     Draw(size, count);
+}
+
+function PlayPause()
+{
+    gameState++;
+    gameState%=2;
+}
+
+function SetCell()
+{
+    if(gameState == 0)
+    {
+        var zone = document.getElementById("game");
+        var size1 = zone.offsetWidth/count;
+        var x = parseInt((event.pageX - zone.offsetLeft)/size1);
+        var y = parseInt((event.pageY - zone.offsetTop)/size1);
+        cells[x][y]++;
+        cells[x][y]%=2;
+        Draw(size, count);
+    }
 }
 
 function Draw(size, count)
@@ -31,11 +155,17 @@ function Draw(size, count)
         }
         else
         {
-            context.fillStyle = '#AF5200';
+            context.fillStyle = '#f88';
         }
         context.fillRect(i * size + 1, j * size + 1, size - 2, size - 2);
     }
 }
 
-var time = new Date().getTime();
-var stepTime = 0.5;
+var timer =  setInterval(function Update()
+{
+    if(gameState != 0)
+    {
+        GameStep();
+    }
+    Draw(size, count);
+}, 300);
